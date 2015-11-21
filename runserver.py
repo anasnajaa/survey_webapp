4d41798a77046f9154c7d957dc3e31ba90d23b33
Anas Najaa <anas@najaa.io>
2015-11-21 20:03:13 +0300

    Deleted comments
    Will master accept the changes automatically after pulling?

--- runserver.py
+++ runserver.py
@@ -7,15 +7,6 @@ This script runs the FlaskWebProject1 application using a development server.
 from os import environ
 from __init__ import app
 
-#Use this part for development env in Visual Studio
-#if __name__ == '__main__':
-#    HOST = environ.get('SERVER_HOST', 'localhost')
-#    try:
-#        PORT = int(environ.get('SERVER_PORT', '5555'))
-#    except ValueError:
-#        PORT = 5555
-#    app.run(HOST, PORT)
 
-#Use this part for production env (currently using redis/flask/nginx/gunicorn)
 if __name__ == '__main__':
     app.run()
